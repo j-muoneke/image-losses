d3871b125fc619de66dd097e2737395eab192cef
George Ogden <george-ogden@outlook.com>
2024-11-29 08:02:10 +0000

    copy across resnet subset

--- cnn_loss/cnn_loss.py
+++ cnn_loss/cnn_loss.py
@@ -1,9 +1,28 @@
 # imports
 import torch.nn as nn
 
-# ResNet wrapper class
-class ResNetSubset():
-    pass
+class ResNetSubset(nn.Module):
+    """Store a subset of ResNet model weights to reduce memory usage."""
+    def __init__(self, resnet):
+        super().__init__()
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu
+        self.maxpool = resnet.maxpool
+        self.layer1 = resnet.layer1
+
+        for param in self.parameters():
+            param.requires_grad_(False)
+    
+    def forward(self, x):
+        early_features = self.conv1(x)
+        x = self.bn1(early_features)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        mid_features = self.layer1(x)
+        return early_features, mid_features
+
 
 # CNN Loss class
 class CNNLoss(nn.Module):
