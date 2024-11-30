9359e3e0c270536604b431365f6e37df4009c9c2
George Ogden <george-ogden@outlook.com>
2024-11-30 12:24:15 +0000

    fix typos

--- wavelet_loss/wavelet_loss/wavelet_loss.py
+++ wavelet_loss/wavelet_loss/wavelet_loss.py
@@ -25,10 +25,10 @@ class WaveletLoss(nn.Module):
         pred_yl, pred_yh = pred_coeffs
         target_yl, target_yh = target_coeffs
 
-        for pred_coeffs, targte_coeffs in zip(pred_yl, target_yl):
-            total_loss += self.loss_fn(pred_coeffs, targte_coeffs) * self.loss_coeffs[0]
+        for pred_coeffs, target_coeffs in zip(pred_yl, target_yl):
+            total_loss += self.loss_fn(pred_coeffs, target_coeffs) * self.loss_coeffs[0]
 
-        for pred_coeffs, targte_coeffs in zip(pred_yh, target_yh):
-            total_loss += self.loss_fn(pred_coeffs, targte_coeffs) * self.loss_coeffs[1]
+        for pred_coeffs, target_coeffs in zip(pred_yh, target_yh):
+            total_loss += self.loss_fn(pred_coeffs, target_coeffs) * self.loss_coeffs[1]
 
         return total_loss
